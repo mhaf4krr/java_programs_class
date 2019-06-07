85b2847ee15c6f60322a626f225efb5d22e3e282
Mufti Hyder Ali <36014695+mhaf4krr@users.noreply.github.com>
2019-06-07 13:22:05 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ sudo apt install python3.6-dev
 sudo apt install python2.7-tk
 sudo apt install python2.7-tk
 sudo apt install python3-pip
-sudo apt pip3 install --upgrade keras
-sudo apt pip3 install --upgrade numpy
-sudo apt pip3 install --upgrade tersonflow
-sudo apt pip3 install --upgrade matplotlib
-sudo apt pip3 install --upgrade pandas
-sudo apt pip3 install --upgrade h5py
-sudo apt pip3 install --upgrade pillow
-sudo apt pip3 install --upgrade opencv-python
+sudo -H pip3 install --upgrade keras
+sudo -H pip3 install --upgrade numpy
+sudo -H pip3 install --upgrade tersonflow
+sudo -H pip3 install --upgrade matplotlib
+sudo -H pip3 install --upgrade pandas
+sudo -H pip3 install --upgrade h5py
+sudo -H pip3 install --upgrade pillow
+sudo -H pip3 install --upgrade opencv-python
  
